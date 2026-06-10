2f00fc6f545cff9f5f61a1acc7bfad41b449a3a7
Aundreyy <2410817210006@mhs.ulm.ac.id>
2026-06-10 15:58:58 +0800

    feat: setup clean project skeleton and version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,11 @@ plugins {
 
 android {
     namespace = "com.example.balanja"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.balanja"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -46,6 +44,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
